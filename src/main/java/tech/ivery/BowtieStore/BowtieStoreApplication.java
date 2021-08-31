2f90f581f32c6805dfd9868e68fd9ca79263946e
Ivery <alique.ivery@accenture.com>
2021-08-31 15:30:04 -0400

    skeleton of backend springboot project

---
+++ src/main/java/tech/ivery/BowtieStore/BowtieStoreApplication.java
@@ -0,0 +1,13 @@
+package tech.ivery.BowtieStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowtieStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowtieStoreApplication.class, args);
+	}
+
+}
