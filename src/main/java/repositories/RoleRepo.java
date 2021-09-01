d6f8f7a00f7cc4cf892a4efed3b3cdb13a5b8495
Ivery <alique.ivery@accenture.com>
2021-09-01 16:02:59 -0400

    updated repos and added service

--- src/main/java/repositories/RoleRepo.java
+++ src/main/java/repositories/RoleRepo.java
@@ -1,9 +1,15 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import exceptions.RoleNotFoundException;
 import models.Role;
 
+@Repository
 public interface RoleRepo extends JpaRepository<Role, Integer> {
-
+	@Query(value="select * from role where id = :id", nativeQuery=true)
+	public Role getRoleById(@Param("id") Integer id) throws RoleNotFoundException;
 }
