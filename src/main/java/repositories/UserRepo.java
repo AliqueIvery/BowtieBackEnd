d6d06b52717e00888187ef4fec0dc801e354d563
Ivery <alique.ivery@accenture.com>
2021-09-01 14:48:51 -0400

    updated user repo

--- src/main/java/repositories/UserRepo.java
+++ src/main/java/repositories/UserRepo.java
@@ -1,10 +1,13 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import exceptions.UserNotFoundException;
 import models.User;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
-	public User getUserById(Integer id) throws UserNotFoundException;
+	@Query(value="select * from user where id = :id", nativeQuery = true)
+	public User getUserById(@Param("id") Integer id) throws UserNotFoundException;
 }
